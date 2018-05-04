10ed2a637516648a3a2b399425d58ad31b2b6a66
Lapo Luchini <lapo@lapo.it>
2018-05-04 20:54:58 +0200

    Remove unused imports.

--- WindowsLayoutSnapshot/TrayIconForm.cs
+++ WindowsLayoutSnapshot/TrayIconForm.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Forms;
 
 namespace WindowsLayoutSnapshot {
